f4d6d4b3c468992fbad558ce12167bb0261e5d7b
Yash Jasani <jasaniyash13@gmail.com>
2023-06-27 11:07:24 +0530

    changed index.html file

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Temp from "./component/weather/temp";
+// import Temp from "./component/weather/temp";
 // import Restaurant from "./component/Basics/Restaurant";
 // import UseState from "./component/Hooks/useState";
 // import UseEffect from "./component/Hooks/useEffect";
 // import UseReducer from "./component/Hooks/useReducer";
-// import Todo from "./component/todoReact/todo";
+import Todo from "./component/todoReact/todo";
 
 const App = () => {
   return (
     <>
-      <Temp></Temp>
+      <Todo></Todo>
     </>
   );
 };
